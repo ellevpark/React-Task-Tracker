4a9fbfdefe56ac0e28c3df988a3930cbcaa2fa0d
Ellen Park <ellenpark@Ellens-MacBook-Pro.local>
2021-08-19 09:42:00 -0400

    created a component for Button

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,18 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({title}) => {
+
+  const onClick = () => {
+    console.log("Click!")
+  }
     return (
-      <header className= 'header'>
-        <h1>{title}</h1>
-        <Button color='blue' text='Hi'/>
-      </header>
+      <div className = 'container'>
+        <header className= 'header'>
+          <h1>{title}</h1>
+          <Button color='blue' text='Add' onClick = {onClick}/>
+        </header>
+      </div>
+      
     )
 }
 
