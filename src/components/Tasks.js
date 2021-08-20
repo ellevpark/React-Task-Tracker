5013e3d4bbb3f8d72d17f6e39775bb2957952248
Ellen Park <ellenpark@Ellens-MacBook-Pro.local>
2021-08-20 11:05:52 -0400

    created Task component for each individual task

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import Task from './Task'
 
 const Tasks = ({tasks}) => {
   
   return (
       <>
         {tasks.map((task) => (
-          <h3 key = {task.id}>{task.text}</h3>
+          <Task key = {task.id} task = {task}/>
         ))}
       </>
   )
