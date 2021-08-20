514531ceaa35e7888dd879913961b4600ea764db
Ellen Park <ellenpark@Ellens-MacBook-Pro.local>
2021-08-20 11:15:51 -0400

    downloaded react-icons and added a delete icon to each task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,12 @@
-
-import React from 'react'
+import {FaTimes} from 'react-icons/fa'
 
 const Task = ({task}) => {
     return (
         <div className = 'task'>
-            <h3>{task.text}</h3>
+            <h3>
+                {task.text} <FaTimes/>
+            </h3>
+            <p>{task.day}</p>
         </div>
     )
 }
