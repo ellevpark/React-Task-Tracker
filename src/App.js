66af50cdb58bcd55e1ac5fd61a9600d5ba67f4a2
Ellen Park <ellenpark@Ellens-MacBook-Pro.local>
2021-08-23 13:32:10 -0400

    created toggle functions to show and hide add task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 
-function App() {
+const App=() => {
+
+  const [showAddTask,  setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState( 
     [ 
@@ -33,20 +36,27 @@ function App() {
   }
 
   const toggleReminder = (id) => {
-    console.log('hi')
     setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task
-
     )
     )
   }
 
+  const addTask = (task) => {
+
+    const id = Math.floor(Math.random +10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   return (
     <div className="container">
-      <Header/>
+      <Header onAdd = {() => {setShowAddTask(!showAddTask)}} showAdd = {showAddTask}/>
+      {showAddTask && <AddTask onAdd = {addTask}/>}
       {tasks.length > 0 
       ? <Tasks tasks = {tasks} 
       onDelete = {deleteTask} 
-      onToggle = {toggleReminder}/> 
+      onToggle = {toggleReminder}
+      /> 
       : 'All Tasks Completed'}
     </div>  
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({title}) => {
+const Header = ({title, onAdd, showAdd}) => {
 
-  const onClick = () => {
-    console.log("Click!")
-  }
-    return (
-      <header className= 'header'>
-        <h1>{title}</h1>
-        <Button color='green' text='Add' onClick = {onClick}/>
-      </header>
-      
-    )
+  return (
+    <header className= 'header'>
+      <h1>{title}</h1>
+      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick = {onAdd}/>
+    </header>
+    
+  )
 }
 
 Header.defaultProps = {
